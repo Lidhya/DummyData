1d6cb56711f550501fdb8bc856c4839e9e1cb9a8
Lidhya <lidhyajeejan@gmail.com>
2023-05-26 20:42:28 +0530

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 5000;
+
+app.use(cors());
 
 // Generate random user data
 const generateUser = () => {
